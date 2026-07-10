70b35e02e1c52f134f102c99469fd3674a779b72
Apra Khanna <aprakhanna5@gmail.com>
2026-07-10 17:35:29 +0100

    Add SSE endpoint with simulated live meeting data and disconnect cleanup

--- server.js
+++ server.js
@@ -12,4 +12,25 @@ const app = express();
 // Your function calls response.send('Hello World!') → this text gets sent back to the browser.
 
 app.use(express.static('public'));
+app.get('/events',(request, response)=>{
+    response.writeHead(200, {
+   'Content-Type': 'text/event-stream',
+'Cache-Control': 'no-cache',
+'Connection': 'keep-alive'
+});
+let voteCount=0;
+const intervalId=setInterval(()=>{
+const peopleArray=["abc","john","lisa","mony","leslie","joy","bob"];
+let currentObj={ currentSpeaker: "", participantCount:0,  voteCount:0  }
+let someRandomIndex=Math.floor(Math.random()*peopleArray.length)
+currentObj.currentSpeaker=peopleArray[someRandomIndex]
+currentObj.participantCount=Math.floor(Math.random() * 20+5);
+  voteCount++; 
+  currentObj.voteCount = voteCount; 
+response.write("data: " + JSON.stringify(currentObj) + "\n\n")
+},3000)
+request.on('close', () => { clearInterval(intervalId)}) 
+//this registers a listener that fires automatically when the client disconnects.
+})
 app.listen(3000);
+//Server: real object → JSON.stringify → text string → sent over network → Browser: receives text string → JSON.parse → real object again.
